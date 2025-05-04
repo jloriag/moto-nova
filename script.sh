8caffad1f0c12347ac75370891ea829a2b44b909
Josue Loria <jloriag@hotmail.com>
2025-05-04 16:23:29 -0600

    changes

--- script.sh
+++ script.sh
@@ -10,3 +10,7 @@ unzip latest.zip
 mv wordpress /var/www/html/
 sudo chown -R www-data:www-data /var/www/html/wordpress
 sudo chmod -R 755 /var/www/html/wordpress
+
+sudo a2ensite wordpress.conf
+sudo a2enmod rewrite
+sudo systemctl reload apache2
---
+++ script.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE USER 'wpuser'@'localhost' IDENTIFIED BY 's9enD&l)3tgin';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'localhost';
+FLUSH PRIVILEGES;
+EXIT;
