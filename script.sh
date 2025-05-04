e67e0ffdbe7edf4f44500e18dc6ed8966d82bec1
Josue Loria <jloriag@hotmail.com>
2025-05-04 16:21:20 -0600

    Changes

--- script.sh
+++ script.sh
@@ -3,4 +3,10 @@
 
 echo "Hola, mundo"
 sudo apt update
-date
+sudo apt install apache2 mysql-server php php-mysql libapache2-mod-php php-cli php-curl php-gd php-xml php-mbstring php-zip php-soap unzip -y
+cd /tmp
+wget https://wordpress.org/latest.zip
+unzip latest.zip
+mv wordpress /var/www/html/
+sudo chown -R www-data:www-data /var/www/html/wordpress
+sudo chmod -R 755 /var/www/html/wordpress
