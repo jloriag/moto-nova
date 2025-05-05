dddaf5d238395d5a84887829f492ad11218b58fc
Josue Loria <jloriag@hotmail.com>
2025-05-05 14:10:37 -0600

    changes

--- wordpress/wordpress.sh
+++ wordpress/wordpress.sh
@@ -2,8 +2,8 @@ cd /tmp
 wget https://wordpress.org/latest.zip
 unzip latest.zip
 mv wordpress /var/www/html/
-sudo chown -R www-data:www-data /var/www/html/wordpress
-sudo chmod -R 755 /var/www/html/wordpress
+chown -R www-data:www-data /var/www/html/wordpress
+chmod -R 755 /var/www/html/wordpress
 
 
 #!/bin/bash
