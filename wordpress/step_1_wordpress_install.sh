9a76f8869bac574f050a9bccc05139d8674eb91f
Josue Loria <jloriag@hotmail.com>
2025-05-07 11:11:36 -0600

    Cambiar a ruta especifica

--- wordpress/step_1_wordpress_install.sh
+++ wordpress/step_1_wordpress_install.sh
@@ -1,9 +1,5 @@
-cd /tmp
-wget https://wordpress.org/latest.zip
-unzip latest.zip
-mv wordpress /var/www/html/
+wget -P /tmp https://wordpress.org/latest.zip
+unzip latest.zip -d /tmp
+mv /tmp/wordpress /var/www/html/
 chown -R www-data:www-data /var/www/html/wordpress
 chmod -R 755 /var/www/html/wordpress
-sudo a2ensite wordpress.conf
-sudo a2enmod rewrite
-sudo systemctl reload apache2
