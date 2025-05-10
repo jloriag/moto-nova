0b5c435dc168ad02b444c07d48cd4b856f069e37
Josue Loria <jloriag@hotmail.com>
2025-05-10 13:46:25 -0600

    --path=/var/www/html added

--- wordpress/step_5_woocommerce.sh
+++ wordpress/step_5_woocommerce.sh
@@ -2,10 +2,10 @@
 
 sudo -u www-data wp plugin install woocommerce --activate --path=/var/www/html
 
-sudo -u www-data wp option update woocommerce_store_address "Ipis"
-sudo -u www-data wp option update woocommerce_store_city "San José"
-sudo -u www-data wp option update woocommerce_store_postcode "10101"
-sudo -u www-data wp option update woocommerce_store_country "CR"
-sudo -u www-data wp option update woocommerce_store_state "CR-SJ"
-sudo -u www-data wp option update woocommerce_currency "CRC"
-sudo -u www-data wp option update woocommerce_product_type "both"
+sudo -u www-data wp option update woocommerce_store_address "Ipis" --path=/var/www/html
+sudo -u www-data wp option update woocommerce_store_city "San José" --path=/var/www/html
+sudo -u www-data wp option update woocommerce_store_postcode "10101" --path=/var/www/html
+sudo -u www-data wp option update woocommerce_store_country "CR" --path=/var/www/html
+sudo -u www-data wp option update woocommerce_store_state "CR-SJ" --path=/var/www/html
+sudo -u www-data wp option update woocommerce_currency "CRC" --path=/var/www/html
+sudo -u www-data wp option update woocommerce_product_type "both" --path=/var/www/html
