6c0305b6d6905666d834287a7aa544136a467520
Josue Loria <jloriag@hotmail.com>
2025-05-11 10:23:28 -0600

    sudo added

--- wordpress/step_2_wordpress_download.sh
+++ wordpress/step_2_wordpress_download.sh
@@ -3,5 +3,5 @@
 mkdir -p /var/www/.wp-cli/cache
 chown -R www-data:www-data /var/www/.wp-cli
 chown -R www-data:www-data /var/www/html
--u www-data wp core download --path=/var/www/html
+sudo -u www-data wp core download --path=/var/www/html
 rm /var/www/html/index.html
