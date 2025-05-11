0c5f7f774e87fb921a552309faf4ab5e28b77dba
Josue Loria <jloriag@hotmail.com>
2025-05-11 10:04:56 -0600

    Adding  SCRIPT_PATH

--- wordpress/full_steps_wordpress.sh
+++ wordpress/full_steps_wordpress.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-chmod +x step_1_wp_cli.sh
-chmod +x step_2_wordpress_download.sh
-chmod +x step_3_wordpress_db.sh
-chmod +x step_4_wordpress_install.sh
-chmod +x step_5_woocommerce.sh
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+chmod +x "$SCRIPT_PATH/step_1_wp_cli.sh"
+chmod +x "$SCRIPT_PATH/step_2_wordpress_download.sh"
+chmod +x "$SCRIPT_PATH/step_3_wordpress_db.sh"
+chmod +x "$SCRIPT_PATH/step_4_wordpress_install.sh"
+chmod +x "$SCRIPT_PATH/step_5_woocommerce.sh"
 
-source step_1_wp_cli.sh
-source step_2_wordpress_download.sh
-source step_3_wordpress_db.sh
-source step_4_wordpress_install.sh
-source step_5_woocommerce.sh
+source "$SCRIPT_PATH/source step_1_wp_cli.sh"
+source "$SCRIPT_PATH/step_2_wordpress_download.sh"
+source "$SCRIPT_PATH/step_3_wordpress_db.sh"
+source "$SCRIPT_PATH/step_4_wordpress_install.sh"
+source "$SCRIPT_PATH/step_5_woocommerce.sh"
