1f133d7642d5d5f3e2729bfb6876208b5c4e9e04
Josue Loria <jloriag@hotmail.com>
2025-05-11 10:11:20 -0600

    fix

--- wordpress/full_steps_wordpress.sh
+++ wordpress/full_steps_wordpress.sh
@@ -6,7 +6,7 @@ chmod +x "$SCRIPT_PATH/step_3_wordpress_db.sh"
 chmod +x "$SCRIPT_PATH/step_4_wordpress_install.sh"
 chmod +x "$SCRIPT_PATH/step_5_woocommerce.sh"
 
-source "$SCRIPT_PATH/source step_1_wp_cli.sh"
+source "$SCRIPT_PATH/step_1_wp_cli.sh"
 source "$SCRIPT_PATH/step_2_wordpress_download.sh"
 source "$SCRIPT_PATH/step_3_wordpress_db.sh"
 source "$SCRIPT_PATH/step_4_wordpress_install.sh"
